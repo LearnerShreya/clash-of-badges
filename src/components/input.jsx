7a8777e8e2d2d545ed0f7660a72e756c8f1b6a98
Shreya Singh <shreya.official.2.25@gmail.com>
2025-06-24 18:39:32 +0530

    new file:   src/components/input.jsx

---
+++ src/components/input.jsx
@@ -0,0 +1,12 @@
+// src/components/input.jsx
+
+import React from "react";
+
+export function Input(props) {
+  return (
+    <input
+      {...props}
+      className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+    />
+  );
+}
